337c5234276192ad0d8109588f5f86085f822e24
manuelcarmona <manucarcor@gmail.com>
2018-05-09 16:46:12 +0200

    build kernel with clang & Snap overlay fix
    Change-Id: I74a10e842be8aab3a79c5fb336d2e4f0d88c7298

--- board/kernel.mk
+++ board/kernel.mk
@@ -6,3 +6,18 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8917
 TARGET_USE_SDCLANG := true
+
+# ExFAT target
+TARGET_EXFAT_DRIVER := exfat
+
+# Kernel Clang
+TARGET_KERNEL_CLANG_COMPILE := true
+KERNEL_CLANG_VERSION := sd4.0
+TARGET_KERNEL_CLANG_PATH:= $(ANDROID_BUILD_TOP)/prebuilts/clang/linux-x86/host/clang-sd4.0/bin
+
+# ROM Clang
+ifneq ($(HOST_OS),darwin)
+  SDCLANG := true
+  SDCLANG_PATH := prebuilts/clang/linux-x86/host/clang-sd4.0/bin
+  SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
+endif
